587ebd45871291a9b4c23098df753e60f655deed
Vlad Grey <Vlad9rey@gmail.com>
2026-01-15 23:53:54 +0200

    Upgrade major dependencies and refactor for compatibility

---
+++ prisma.config.ts
@@ -0,0 +1,14 @@
+// prisma.config.ts
+import "dotenv/config";
+import path from "node:path";
+import { defineConfig, env } from "prisma/config";
+
+export default defineConfig({
+  schema: path.join("prisma", "schema.prisma"),
+  migrations: {
+    path: path.join("prisma", "migrations"),
+  },
+  datasource: {
+    url: env("DATABASE_URL"),
+  },
+});
